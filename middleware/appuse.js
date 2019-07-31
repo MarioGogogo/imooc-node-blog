df24b84060ef02055982652f44a3e10eb1b66552
Mario <148373644@qq.com>
2019-07-31 23:26:14 +0800

    add htmlpages 😄

---
+++ middleware/appuse.js
@@ -0,0 +1,18 @@
+const http = require('http')
+const slice = Array.prototype.slice
+
+// app.use('/api',(req,res,next)=>{})
+// app.use((req,res,next)=>{})
+
+
+class LikeExpress{
+
+
+}
+
+
+
+
+module.exports = ()=>{
+   return  new LikeExpress()
+}
